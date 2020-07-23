19ebc040a3f868dc3dd449db459f1a73aef6dda9
Tali Avni <tali@aristo7.com>
2020-07-23 15:06:38 -0700

    creating hot nonsense markov quotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default function App() {
+  const ron = require('../data/ron-swanson');
+
+  const Text = require('markov-chains-text').default;
+
+  const generatedSentence = new Text(ron.text);
+
+  console.log(generatedSentence.makeSentence());
+
   return <h1>Hello World</h1>;
 }
-  
---
+++ src/data/ron-swanson.js
@@ -0,0 +1,78 @@
+module.exports = {
+
+  text: 'There’s only one thing I hate more than lying: skim milk. Which is water that is lying about being milk. I’d wish you the best of luck but I believe luck is a concept created by the weak to explain their failures. Dear frozen yogurt, you are the celery of desserts. Be ice cream or be nothing. Zero stars. Tom put all my records into this rectangle. The songs just play one after the other. This is an excellent rectangle. I’m not interested in caring about people. Clear alcohols are for rich women on diets. Crying: Acceptable at funerals and the Grand Canyon. There are three acceptable haircuts: high and tight, crew cut, buzz cut. There are only three ways to motivate people: money, fear, and hunger. The key to burning an ex-wife effigy is to dip it in paraffin wax and then toss the flaming bottle of isopropyl alcohol from a safe distance. Do not stand too close when you light an ex-wife effigy. Under my tutelage, you will grow from boys to men. From men into gladiators. And from gladiators into Swansons. People are idiots, Leslie. Great job, everyone. The reception will be held in each of our individual houses, alone. On my deathbed, my final wish is to have my ex-wives rush to my side so I can use my dying breath to tell them both to go to hell one last time. Normally, if given the choice between doing something and nothing, I’d choose to do nothing. But I will do something if it helps someone else do nothing. I’d work all night, if it meant nothing got done. Never half-ass two things. Whole-ass one thing. Any dog under fifty pounds is a cat and cats are useless. There is only one bad word: taxes. Friends: one to three is sufficient. Breakfast food can serve many purposes. Fishing is for sport only. Fish meat is practically a vegetable. I’ll take that steak to go. Please and thank you. I was born ready. I’m Ron Fucking Swanson. My idea of a perfect government is one guy who sits in a small room at a desk, and the only thing he’s allowed to decide is who to nuke. The man is chosen based on some kind of IQ test, and maybe also a physical tournament, like a decathlon. And women are brought to him, maybe…when he desires them. Capitalism: God’s way of determining who is smart and who is poor. The less I know about other people’s affairs, the happier I am. I’m not interested in caring about people. I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes. Fishing relaxes me. It’s like yoga, except I still get to kill something. Just give me all the bacon and eggs you have. Wait … I worry what you heard was, ‘Give me a lot of bacon and eggs.’ What I said was, give me all the bacon and eggs you have. Do you understand? Don’t start chasing applause and acclaim. That way lies madness. I call this turf ‘n’ turf. It’s a 16-ounce T-bone and a 24-ounce porterhouse. Also, whiskey and a cigar. I am going to consume all of this at the same time because I am a free American. I’m a simple man. I like pretty, dark-haired women and breakfast food. That is a canvas sheet, the most versatile object known to man. It can be used to make tents, backpacks, shoes, stretchers, sails, tarpaulins, and I suppose, in the most dire of circumstances, it can be a surface on which to make art. If there were more food and fewer people, this would be a perfect party. When people get too chummy with me, I like to call them by the wrong name to let them know I don’t really care about them. So you talked to Tammy? What’s it like to stare into the eye of Satan’s butthole?  My idea of a perfect government is one guy who sits in a small room at a desk, and the only thing he’s allowed to decide is who to nuke. The man is chosen based on some kind of IQ test, and maybe also a physical tournament, like a decathlon. And women are brought to him, maybe…when he desires them. Turkey can never beat cow. Breakfast food can serve many purposes. I like saying no, it lowers their enthusiasm. You had me at Meat Tornado. Are you going to tell a man that he can’t fart in his own car? Put some alcohol in your mouth to block with words from coming out. Give 100%. 110% is impossible. Only idiots recommend that. Birthdays were invented by Hallmark to sell cards. When I eat, it is the food that is scared. Sting like a bee. Do not float like a butterfly. That’s ridiculous. Give a man a fish and feed him for a day. Don’t teach a man to fish…and feed yourself. He’s a grown man. And fishing’s not that hard. America: The only country that matters. If you want to experience other ‘cultures,’ use an atlas or a ham radio. History began on July 4, 1776. Everything that happened before that was a mistake. I don’t want to paint with a broad brush here, but every single contractor in the world is a miserable, incompetent thief. Straight down the middle. No hook, no spin, no fuss. Anything more and this becomes figure skating. The government is a greedy piglet that suckles on a taxpayer’s teat until they have sore, chapped nipples. If any of you need anything at all, too bad. Deal with your problems yourselves, like adults. When people get too chummy with me I like to call them by the wrong name to let them know I don’t really care about them. Encapsulate the spirit of melancholy. Easy. Boom, a sad desk. Boom, sad wall. It’s art. Anything is anything. Listen, I’ve eaten a commissary hamburger for lunch every day for twelve years. I just wanted to make sure this pointless health crusade won’t affect the only part of my job that I like. This is a flying robot that I just shot out of the sky when it tried to deliver me a package. If it doesn’t have meat, it’s a snack. In my opinion, not enough people have looked their dinner in the eyes and considered the circle of life. We will get along just fine, though hopefully not too fine, because I am not looking for any new friends. End speech. Haha, Euro-trash, I like that. That is indeed a garbage continent. Creativity is for people with glasses who like to lie. Say what you want about organized religion, but those bastards knew how to construct an edifice. Is Star Wars the one with the wizard boy? I would rather bleed out than sit here and talk about my feelings for 10 mins. I prefer quality over flash – that’s why I refuse to write my signature in cursive. Live your life how you want, but don’t confuse drama with happiness. On nights like this when the cold winds blow, the air is awash in the swirling eddies of our dream, come with me and find safe haven in a warm bathtub full of my jazz. My son is several weeks old. He is very familiar with the sound of power tools. I regret nothing. The end.'
+
+  // 55. One rage every three months is permitted. Try not to hurt anyone who doesn’t deserve it.
+
+  // 56. Strippers do nothing for me…but I will take a free breakfast buffet anytime, anyplace.
+
+  // 57. No home is complete without a proper toolbox. Here’s April and Andy’s: A hammer, a half-eaten pretzel, a baseball card, some cartridge that says Sonic and Hedgehog, a scissor half, a flashlight filled with jellybeans.
+
+
+  // 58.My first ex-wife’s name is Tammy. My second ex-wife’s name is Tammy. My Mom’s name is Tamara…she goes by Tammy.
+
+  // 59. I’ve cried twice in my life. Once when I was 7 and hit by a school bus. And then again when I heard that Li’l Sebastian had passed.
+
+  // 60. There has never been a sadness that can’t been cured by breakfast food.
+
+  // 61. If any of you need anything at all, too bad. Deal with your problems yourselves, like adults.
+
+  // 62. OK, everyone: shut up! And look at me!
+
+  // 63. Welcome to ‘Visions of Nature.’ This room has several paintings in it. Some are big, some are small. People did them and they’re here now. I believe that after this is over, they’ll be hung in government buildings. Why the government is involved in an art show is beyond me. I also think it’s pointless for a human to paint scenes of nature when they could just go outside and stand in it. Anyway, please do not misinterpret the fact that I am talking right now as genuine interest in art and attempt to discuss it with me further. End of speech.
+
+
+  // 64. Do you have any history of mental illness in your family? I have an uncle who does yoga.
+
+  // 65.Busy? Impossible. I work for the government.
+
+  // 66. Child labor laws are ruining this country.
+
+  // 67. I think that all government is a waster of taxpayer money. My dream is to have the park system privatized and run entirely by for profit corporations, like Chuck E. Cheese. They have an impeccable business model.
+
+  // 68. When I walked in this morning and saw that the flag was half-mast, I thought ‘All right, another bureaucrat ate it!
+
+  // 69. I like Tom. He doesn’t do a lot of work around here. He shows zero imitative. He’s not a team player. He’s never wanted to go that extra mile. Tom is exactly what I’m looking for in a government employee.
+
+  // 70. The whole point of this country is if you want to eat garbage, balloon up to 600 pounds and die of a heart attack at 43, you can! You are free to do so. To me, that’s beautiful.
+
+
+  // 71. Capitalism is the only way … It makes America great, England OK and France terrible.
+
+  // 72. [After being asked if he wants a salad]  Since I am not a rabbit, no I do not.
+
+  // 73. You’ve accidentally given me the food that my food eats.
+
+  // 74.  I’ve created this office as a symbol of how I feel about government. This sawed-off shotgun belonged to a local bootlegger. People who come in here to ask me for things have to stare right down the barrel…
+
+  // 75. I love being a father but there are a few things I miss: Silence. The absence of noise. One single moment undisturbed by the sounds of a children’s program called Doc McStuffins. There is no quiet anymore. There is only Doc McStuffins.
+
+  // ron swanson quotes
+  // NBC
+
+  // 76. I have accrued two hundred and twenty five personal days starting right now I’m using all of them. While I’m gone your in charge. Also, I keep a sizable supply of ground chuck in my desk. Remove it or it will begin to smell. Godspeed.
+
+  // 77. Ron Swanson’s rules for a BBQ: There will be no froof-y desserts. There will be no giant soap bubble guns. There will be no adult men in costumes. And most of all, there will be no f#^%ing vegetables.
+
+  // 78.  I have a hernia. I’ve had it for a while, and I’ve been ignoring it successfully. But uh, this morning, I made the mistake of sneezing. But as long as I sit still and don’t move my head or torso, I’m good. I got this.
+
+  // 79. Passing the Buck. The last refuge of the cowardly and Blackhearted.
+
+  // 80. An ideal night out, to me, is stepping onto my porch area and grilling up a thick slab of something’s flesh and then popping in a highlight real from the WNBA.
+
+
+  // 81. [Describing his allergies] Cowardice and Weak Willed Men… and Hazelnuts
+
+  // 82. [Describing his sexual history]  Epic. and Private.
+
+  // 83. Veganism is the sad result of a morally corrupt mind. Reconsider your life.
+
+  // 84. Barbecues should be about one thing: good shared meat.
+
+  // 85. I work hard to make sure my department is as small and as ineffective as possible.
+
+  // 86. Well, I am not usually one for speeches. So, goodbye.
+
+  // 87. I am submitting this menu from a Pawnee institution, J.J.’s Diner. Home of the world’s best breakfast dish: The Four Horsemeals of the Eggporkalypse.
+
+};
